8a01c5f2c6bfa1bed3dfe679fcca65f307d9e8db
Ishani Arora <ishani.arora2005@gmail.com>
2026-06-09 23:31:34 +0530

    added forward backward functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,43 @@
-const song = new Audio("src/assets/songs_mp3/song1.mp3");
+const image=document.getElementById("image");
+const title=document.getElementById("title");
+const artist=document.getElementById("artist");
+
+const songs = [
+    {
+        title: "Liquid Smooth",
+        artist: "Mitski", 
+        file: "src/assets/songs_mp3/song1.mp3",
+        image:"src/assets/images/songs/song1.png"
+    },
+    {
+        title: "Sweeter Weather",
+        artist: "The Neighbourhood", 
+        file: "src/assets/songs_mp3/song2.mp3",
+        image:"src/assets/images/songs/song2.png"
+    },
+    {
+        title: "Apocalypse",
+        artist: "Cigarettes after Sex", 
+        file: "src/assets/songs_mp3/song3.mp3",
+        image:"src/assets/images/songs/song3.png"
+    }
+]
+
+let currentSong=0;
+const song= new Audio(songs[currentSong].file)
 const progress= document.getElementById("progress");
 const currentTimeEl= document.getElementById("currentTime");
 const durationEl=  document.getElementById("duration");
 
 let isPlaying=false;
 let playicon = document.getElementById("playicon")
+
+function loadSong(index){
+    song.src= songs[index].file;
+    image.src= songs[index].image;
+    title.textContent=songs[index].title;
+    artist.textContent=songs[index].artist;
+}
 
 function actionPlayer(action){
     if (action == "pause"){
@@ -18,6 +51,26 @@ function actionPlayer(action){
             playicon.src = "src/assets/images/buttons/pausee.webp";
             isPlaying=true;
         }
+    }
+    else if(action=="forward"){
+        currentSong++;
+        if (currentSong>=songs.length){
+            currentSong=0;
+        }
+        loadSong(currentSong);
+        song.play();
+        isPlaying=true;
+        playicon.src = "src/assets/images/buttons/pausee.webp";
+    }
+    else if (action=="back"){
+        currentSong--;
+        if (currentSong<0){
+            currentSong=songs.length-1;
+        }
+        loadSong(currentSong);
+        song.play();
+        isPlaying=true;
+        playicon.src = "src/assets/images/buttons/pausee.webp";
     }
 }
 
